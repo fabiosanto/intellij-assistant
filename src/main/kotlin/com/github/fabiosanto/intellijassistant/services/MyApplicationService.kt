4f6fd3ae5e5a32f639439f06893dc7703d6eb456
GitHub Action <action@github.com>
2020-10-18 00:23:05 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/fabiosanto/intellijassistant/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.fabiosanto.intellijassistant.services
+
+import com.github.fabiosanto.intellijassistant.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
